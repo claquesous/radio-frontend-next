75e39379eb0a31819c605f0c93736ee03e81b8ee
Geoff Crew <gc97@cornell.edu>
2024-02-26 02:16:11 -0500

    add footer

---
+++ app/_components/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div className="w-full bg-slate-500 text-slate-200 clear-both p-2">
+      <p>Follow us on <a href="https://musicworld.social/@claqradio">Mastodon</a>.</p>
+      <p>Our space age algorithms are guaranteed to give you the most eclectic mix of alternative music. Powered by <a href="https://github.com/claquesous/radio">ClaqRadio</a>.</p>
+    </div>
+  )
+}
+
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
 import TopBar from './_components/topbar'
+import Footer from './_components/footer'
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Claq Radio",
   description: "Our space age algorithms are guaranteed to give you the most eclectic mix of alternative music.",
-};
+}
 
 export default function RootLayout({
   children,
@@ -24,7 +25,8 @@ export default function RootLayout({
         <div className="pt-16">
           {children}
         </div>
+        <Footer />
       </body>
     </html>
-  );
+  )
 }
