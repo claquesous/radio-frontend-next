3cda56eae642ea5e86f7f817317df63fa5a33af7
Geoff Crew <gc97@cornell.edu>
2024-05-22 11:26:08 -0400

    add player component from stream (#1)

---
+++ app/_components/player.tsx
@@ -0,0 +1,34 @@
+'use client'
+import useSWR from 'swr'
+import { Stream } from '../_types/types'
+
+function getStream() {
+  return fetch('/api/streams')
+    .then(res => {
+      if (!res.ok) {
+        throw new Error('Fetch failed')
+      }
+      return res.json()
+    })
+    .then(streams => {
+      if (streams.length==0) {
+        throw new Error('No streams found')
+      }
+
+      return streams[0]
+    })
+}
+
+export default function Player() {
+  const { data: stream, error, isLoading, isValidating } = useSWR('/api/streams', getStream)
+
+  if (error || isLoading || isValidating) {
+    return <div />
+  }
+
+  return <div className="pt-16">
+    <audio src={`/streams/${stream.name}`} controls preload="none"></audio>
+  </div>
+
+}
+
