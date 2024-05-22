78aecdf26eb88db1fbd7ac5522257f68eebd16e4
Geoff Crew <gc97@cornell.edu>
2024-05-22 11:25:21 -0400

    call api from client side for logged in user

--- app/_components/player.tsx
+++ app/_components/player.tsx
@@ -1,17 +1,30 @@
+'use client'
+import useSWR from 'swr'
 import { Stream } from '../_types/types'
 
-async function getStreams() {
-  const res = await fetch(process.env.RADIO_BACKEND_PATH + '/streams')
+function getStream() {
+  return fetch('/api/streams')
+    .then(res => {
+      if (!res.ok) {
+        throw new Error('Fetch failed')
+      }
+      return res.json()
+    })
+    .then(streams => {
+      if (streams.length==0) {
+        throw new Error('No streams found')
+      }
 
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
-
-  return res.json()
+      return streams[0]
+    })
 }
 
-export default async function Player() {
-  const [stream] = await getStreams()
+export default function Player() {
+  const { data: stream, error, isLoading, isValidating } = useSWR('/api/streams', getStream)
+
+  if (error || isLoading || isValidating) {
+    return <div />
+  }
 
   return <div className="pt-16">
     <audio src={`/streams/${stream.name}`} controls preload="none"></audio>
