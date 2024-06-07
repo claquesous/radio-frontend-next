6a36fb1bc81d54d1cc71c2c8ae501172189e4b9c
Geoff Crew <gc97@cornell.edu>
2024-06-07 00:08:48 -0400

    add station link to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (<>
     Welcome to My Claqradio Rocks!
+
+    <div>
+      <Link href='/s/1'>Listen Now!</Link>
+    </div>
   </>)
 }
 
