576c63492ed1366da65e408f1efcb606cffb2435
Geoff Crew <gc97@cornell.edu>
2025-06-01 14:41:16 -0400

    missing params await

--- app/s/[streamId]/layout.tsx
+++ app/s/[streamId]/layout.tsx
@@ -2,13 +2,13 @@ import dynamic from 'next/dynamic'
 
 const DynamicPlayer = dynamic(() => import('./_components/player'))
 
-export default function StreamsLayout({
+export default async function StreamsLayout({
   children, params
 }: Readonly<{
   children: React.ReactNode,
-  params: { streamId: number },
+  params: Promise<{ streamId: number }>,
 }>) {
-  const { streamId } = params
+  const { streamId } = await params
   return (<>
     <DynamicPlayer streamId={streamId} />
     <div className="pt-2">
