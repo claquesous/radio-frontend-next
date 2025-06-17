8c67952645c99164bae43d97da66afd5176bbbc9
Geoff Crew <gc97@cornell.edu>
2025-06-16 21:58:07 -0400

    fix centering myself

--- app/_components/top-bar.tsx
+++ app/_components/top-bar.tsx
@@ -6,7 +6,7 @@ import LoginForm from './login-form'
 export default function TopBar() {
   return (
     <>
-      <Link className="py-4 ml-2 text-xl inline-block text-slate-200" href="/">
+      <Link className="ml-2 text-xl inline-block text-slate-200" href="/">
         Claq Radio
       </Link>
       <div className="inline-block float-right">
