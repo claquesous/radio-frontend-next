ce7e58c8ebad9c4a80f97c1d319e9b490dd98ba8
Geoff Crew <gc97@cornell.edu>
2025-06-30 21:24:10 -0400

    Restore

--- app/s/[streamId]/_components/plays.tsx
+++ app/s/[streamId]/_components/plays.tsx
@@ -17,36 +17,16 @@ export default async function Plays(props: {streamId: number}) {
   const {streamId} = props
   const plays = await getPlays(streamId)
 
-  return (
-    <div className="w-full min-h-screen">
-      <table className="w-full text-left border-separate border-spacing-y-2">
-        <thead>
-          <tr>
-            <th className="px-4 py-2 text-slate-700 dark:text-slate-200 font-semibold">Artist</th>
-            <th className="px-4 py-2 text-slate-700 dark:text-slate-200 font-semibold">Song</th>
-            <th className="px-4 py-2 text-slate-700 dark:text-slate-200 font-semibold">Rating</th>
-          </tr>
-        </thead>
-        <tbody>
-          {plays.map((play: Play) =>
-            <tr key={play.id} className="bg-white dark:bg-slate-800 hover:bg-slate-100 dark:hover:bg-slate-700 transition-colors">
-              <td className="px-4 py-2">
-                <Link className="text-blue-700 dark:text-blue-300 hover:underline"
-                  href={`/s/${streamId}/artists/${play.artist.id}`}>{play.artist.name}
-                </Link>
-              </td>
-              <td className="px-4 py-2">
-                <Link className="text-slate-900 dark:text-slate-100 hover:underline"
-                  href={`/s/${streamId}/songs/${play.song.id}`}>{play.song.title}
-                </Link>
-              </td>
-              <td className="px-4 py-2">
-                <Rating rating={play.song.rating} />
-              </td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    </div>
-  )
+  return <div className="grid grid-cols-3">{ plays.map((play: Play) =>
+    <Fragment key={play.id}>
+      <Link className="py-7 pl-3"
+        href={`/s/${streamId}/artists/${play.artist.id}`}>{play.artist.name}
+      </Link>
+      <Link className="py-7"
+        href={`/s/${streamId}/songs/${play.song.id}`}>{play.song.title}
+      </Link>
+      <Rating rating={play.song.rating} />
+    </Fragment>
+  ) }</div>
 }
+
