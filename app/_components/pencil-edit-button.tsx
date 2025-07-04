05947985753f96c42440aa98cd7fd3c5de99454a
Geoff Crew <gc97@cornell.edu>
2025-07-04 00:14:39 -0400

    go back...

--- app/_components/pencil-edit-button.tsx
+++ app/_components/pencil-edit-button.tsx
@@ -18,8 +18,7 @@ export default function PencilEditButton({ href }: PencilEditButtonProps) {
       title="Edit"
     >
       <svg xmlns="http://www.w3.org/2000/svg" width={22} height={22} viewBox="0 0 24 24" fill="currentColor" className="text-slate-400 hover:text-blue-500 transition-colors" style={{ display: "inline", verticalAlign: "middle" }}>
-        <polygon points="7,18 17,14 17,7 7,11" />
-        <polygon points="17,7 20,5 10,9 7,11" />
+        <path d="M16.862 5.487a2.25 2.25 0 1 1 3.182 3.182l-9.75 9.75a2 2 0 0 1-.878.513l-4.25 1.062 1.062-4.25a2 2 0 0 1 .513-.878l9.75-9.75z" />
       </svg>
     </Link>
   )
