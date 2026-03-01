828aa3a0be3d837bb06cd03ca6d1fdfdc54088ad
Geoff Crew <gc97@cornell.edu>
2026-03-01 00:17:07 -0500

    upgrade to next 16

--- app/s/[streamId]/layout.tsx
+++ app/s/[streamId]/layout.tsx
@@ -7,14 +7,14 @@ export default async function StreamsLayout({
   children, params
 }: Readonly<{
   children: React.ReactNode,
-  params: Promise<{ streamId: number }>,
+  params: Promise<{ streamId: string }>,
 }>) {
   const { streamId } = await params
   return (<>
-    <DynamicGlobalPlayer streamId={streamId} />
+    <DynamicGlobalPlayer streamId={Number(streamId)} />
     <div className="py-2">
       <div className="mb-4 flex justify-end">
-        <StreamSearchbox streamId={streamId} />
+        <StreamSearchbox streamId={Number(streamId)} />
       </div>
       {children}
     </div>
