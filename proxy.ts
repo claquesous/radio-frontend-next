d6d7f796631431a6316bbaea5e4549cd7f1fc9f5
Geoff Crew <gc97@cornell.edu>
2026-03-01 00:22:01 -0500

    upgrade to next 16 (#42)

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   // Enforce authentication for all /admin routes
   if (request.nextUrl.pathname.startsWith('/admin')) {
     const jwt = request.cookies.get('jwt')
